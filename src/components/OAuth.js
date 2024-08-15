6f76c3430976307bc8feab713970cf8a74331fee
adspohn <spohn.anthony@yahoo.com>
2024-08-15 14:06:51 -0600

    complete the sign in functionality

--- src/components/OAuth.js
+++ src/components/OAuth.js
@@ -1,9 +1,47 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import { getAuth, signInWithPopup } from "firebase/auth";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function OAuth() {
+  const navigate = useNavigate();
+
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      console.log("here");
+      const user = result.user;
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      console.log(user);
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+      console.log(error);
+    }
+  }
+
   return (
-    <button className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+    >
       <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
       Continue with Google
     </button>
